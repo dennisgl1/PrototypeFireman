e1b453a3b8c9a31f9ab42903d97e79e9729d3b99
DennisGL1 <dennis@gamelevelone.com>
2018-01-22 11:35:12 +0700

    - new simulation Scene * 3 objects with different velocity and gravity scale * simulation only!

---
+++ Assets/Scripts/NewBounceObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBounceObject : MonoBehaviour {
+	public Collider2D thisCollider;
+	public Rigidbody2D thisRigidbody;
+
+	public Vector2[] BounceVelocity;
+	public int totalBounce = 0;
+
+	string tagBouncer = "Bouncer";
+	string tagFloor = "Floor";
+	string tagFinish = "Finish";
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == tagBouncer){
+			Bounce();
+		}else if(other.tag == tagFloor){
+			Fall();
+		}else if(other.tag == tagFinish){
+			Finish();
+		}
+	}
+
+	void Bounce()
+	{
+		thisRigidbody.velocity = BounceVelocity[totalBounce];
+		totalBounce++;
+	}
+	void Finish()
+	{
+		Destroy(gameObject);
+	}
+	void Fall()
+	{
+		Destroy(gameObject);
+	}
+
+}
---
+++ Assets/Scripts/UI/ButtonSimulation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSimulation : MonoBehaviour {
+	public GameObject[] prefabNewBounceObjects;
+	public Transform bounceObjectParent;
+	public void ButtonInstantiateOnClick(int index)
+	{
+		Instantiate(prefabNewBounceObjects[index],bounceObjectParent);
+	}
+}
